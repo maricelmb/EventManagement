417dd52c2301591012624f995e1a623cb30dc05b
maricel <maricelmb@gmail.com>
2022-07-25 20:22:47 +0800

    S2L11_Creating domain entity and addin db context

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
