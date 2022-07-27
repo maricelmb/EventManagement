02a48e3bde1591867a1aa85d9073a497b7dbb7c6
maricel <maricelmb@gmail.com>
2022-07-27 22:55:58 +0800

    Added controllers

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,53 @@
+﻿//using Domain;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.EntityFrameworkCore;
+//using Persistence;
+
+//// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+//namespace API.Controllers
+//{
+//    //[Route("api/[controller]")]
+//    [ApiController]
+//    public class ActivitiesController : BaseApiController
+//    {
+//        private readonly DataContext _context;
+
+//        public ActivitiesController(DataContext context)
+//        {
+//            _context = context;
+//        }
+
+//        // GET: api/<ActivitiesController>
+//        [HttpGet]
+//        public async Task<ActionResult<List<Activity>>> GetActivities()
+//        {
+//            return await _context.Activities.ToListAsync();
+//        }
+
+//        // GET api/<ActivitiesController>/5
+//        [HttpGet("{id}")]
+//        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+//        {
+//            return await _context.Activities.FindAsync(id); 
+//        }
+
+//        //// POST api/<ActivitiesController>
+//        //[HttpPost]
+//        //public void Post([FromBody] string value)
+//        //{
+//        //}
+
+//        //// PUT api/<ActivitiesController>/5
+//        //[HttpPut("{id}")]
+//        //public void Put(int id, [FromBody] string value)
+//        //{
+//        //}
+
+//        //// DELETE api/<ActivitiesController>/5
+//        //[HttpDelete("{id}")]
+//        //public void Delete(int id)
+//        //{
+//        //}
+//    }
+//}
