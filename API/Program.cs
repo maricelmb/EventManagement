ff3ad5365750b6054404a45d4d001d46748e521e
maricel <maricelmb@gmail.com>
2022-07-28 10:01:25 +0800

    S3L28: Add cors policy

--- API/Program.cs
+++ API/Program.cs
@@ -16,6 +16,15 @@ builder.Services.ConfigurePersistenceServices(builder.Configuration);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(opt =>
+    opt.AddPolicy("CorsPolicy", policy => 
+    {
+        policy.AllowAnyMethod()
+       .AllowAnyHeader()
+       .WithOrigins("http://localhost:3000");
+    })       
+); 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -28,6 +37,8 @@ if (app.Environment.IsDevelopment())
 //app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors("CorsPolicy");
 
 app.MapControllers();
 
